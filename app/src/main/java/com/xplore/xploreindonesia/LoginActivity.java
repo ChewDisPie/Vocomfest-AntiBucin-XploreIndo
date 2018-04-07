b8a6bc262e234cfcb24836b44940098ebe74dfcf
ilhamdcp <ilhamdcp@gmail.com>
2018-04-08 01:15:10 +0700

    add simple main menu and transition (without UserLoginTask auth)

--- app/src/main/java/com/xplore/xploreindonesia/LoginActivity.java
+++ app/src/main/java/com/xplore/xploreindonesia/LoginActivity.java
@@ -3,6 +3,7 @@ package com.xplore.xploreindonesia;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -184,10 +185,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
-            showProgress(true);
+            //showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            //mAuthTask.execute((Void) null);
+            openMainMenu();
         }
+    }
+
+    private void openMainMenu() {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
     }
 
     private boolean isEmailValid(String email) {
